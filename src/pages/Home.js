66c883a9b4ecfa2799711103480cce18737f51dd
jeonchangbae <avantgard7@gmail.com>
2022-03-26 01:19:18 +0900

    ready to deploy

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -32,7 +32,7 @@ function Home() {
 
   const selectUser = async(user)=>{
     setChat(user);
-    console.log(user);
+
 
     const user2 = user.uid
     const id = user1 > user2 ? `${user1 + user2}` : `${user2+user1}`
@@ -57,8 +57,7 @@ function Home() {
     }
   };
 
-  console.log(msgs);
-
+  
   const handleSubmit = async(e) =>{
     e.preventDefault();
     
